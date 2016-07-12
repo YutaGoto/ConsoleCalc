7efef070d6efd2a3f340b2f041cf71f3893b3c74
YutaGoto <goto@rejob.co.jp>
2016-07-12 13:57:51 +0900

    init commit

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleCalc
+{
+	public class Rule
+	{
+		public int number;
+		public string message;
+
+		public void Init(int num, string msg)
+		{
+			number = num;
+			message = msg;
+		}
+
+		public bool Match(int i)
+		{
+			if (i % number == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public class Application
+	{
+		public Rule[] rule = new Rule[];
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.WriteLine("press any key");
+			Console.ReadKey();
+		}
+	}
+}
