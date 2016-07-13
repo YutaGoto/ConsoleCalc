10eff67d8fcbd9d1adc04f249b95bce3240c6f5d
YutaGoto <you.goto.510@gmail.com>
2016-07-14 00:29:19 +0900

    simple class

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -2,76 +2,43 @@
 using System.Collections.Generic;
 
 namespace ConsoleCalc
-{
-	public class Rule
-	{
-		public int number;
-		public string message;
-
-		public void Init(int num, string msg)
-		{
-			number = num;
-			message = msg;
-		}
-
-		public bool Match(int i)
-		{
-			if (i % number == 0)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
-
-	public class Application
-	{
-		public List<Rule> rule = new List<Rule>();
-
-		public List<Rule> AddRule(Rule r)
-		{
-            return rule.Add(r);
-		}
-
-		public void Run(int start, int stop)
-		{
-            string msg = "";
+{ 
+    public class Application
+    {
+        public void run(int start, int stop)
+        {
+            Console.WriteLine();
             for (int i = start; i <= stop; i++)
-			{
-                foreach (Rule r in rule)
+            {
+                string msg = "";
+                if(i % 3 == 0)
                 {
-                    bool result = r.Match(i);
-                    if (result)
-                    {
-                        msg += "*";
-                    }
-                    else
-                    {
-                        msg = msg + i;
-                    }
+                    msg = msg + "Fizz";
                 }
-			}
-            Console.WriteLine(msg);
-		}
-	}
+
+                if(i % 5 == 0)
+                {
+                    msg = msg + "Buzz";
+                }
+
+                if (msg == "")
+                {
+                    msg = i.ToString();
+                }
+                Console.WriteLine(msg);
+            }
+        }
+    }
 
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-            Rule rule1 = new Rule();
-            Rule rule2 = new Rule();
-            rule1.Init(3, "Fizz");
-            rule2.Init(5, "Buzz");
+            Application game1 = new Application();
 
-            Application game = new Application();
-            game.AddRule(rule1);
-            game.AddRule(rule2);
-
-            game.Run(1, 10);
+            int startNum = int.Parse(Console.ReadLine());
+            int stopNum = int.Parse(Console.ReadLine());
+            game1.run(startNum, stopNum);
 
             Console.WriteLine("press any key");
 			Console.ReadKey();
